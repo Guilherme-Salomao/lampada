94e574c0dd285cad05ff9deec2a7ad14bfbbd23d
Guilherme Salomão Shorane <64329822+Guilherme-Salomao@users.noreply.github.com>
2025-03-05 17:13:40 -0300

    Subindo o projeto

---
+++ assets/js/js.js
@@ -0,0 +1,6 @@
+function ligarLampada() {
+    document.getElementById("lampada").src = "assets/img/ligada.gif";
+}
+function desligarLampada() {
+    document.getElementById("lampada").src = "assets/img/desligada.gif";
+}
